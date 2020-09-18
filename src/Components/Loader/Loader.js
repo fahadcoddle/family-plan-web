611d93c4e92d8154e2fd6a0cb1a6366419fb8e91
Fahad <fahad.d@coddletech.com>
2020-09-18 17:42:05 +0530

    loader changes

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -1,29 +1,25 @@
 import React, { Component } from 'react';
+import 'Components/Loader/Loader.scss';
 import { connect } from 'react-redux';
 import image from 'Assets/785.gif';
 
 class Loader extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
     componentDidMount() {}
 
-    render(){
-        const {serviceReducer} = this.props;
+    render() {
+        const { serviceReducer } = this.props;
         return (
-          <div class="loader-container">  
-            <div class="loader">
-                {serviceReducer.loadings? (
-                <img  src={image} />
-                ): null}
+            <div class="loader-container">
+                <div class="loader">{serviceReducer.loadings ? <img src={image} /> : null}</div>
             </div>
-          </div>  
         );
     }
 }
 
-
-const mapStateToProps = ({serviceReducer}) => ({serviceReducer});
+const mapStateToProps = ({ serviceReducer }) => ({ serviceReducer });
 
 export default connect(mapStateToProps)(Loader);
--- src/pages/Landing/Calender/Calender.js
+++ src/pages/Landing/Calender/Calender.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import 'pages/Landing/Calender/Calender.scss';
 import { Card, Button } from 'antd';
 import Icon, { CalendarOutlined } from '@ant-design/icons';
@@ -6,27 +6,38 @@ import DatePicker from 'Components/Datepicker';
 import ScrollArea from 'react-scrollbar';
 import { getDots, getEventDots, getSchedule } from 'services/calendarService';
 import { addLoading, removeLoading } from 'actions/loaderActions';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 import Loader from 'Components/Loader/Loader';
 
-const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"
+const monthNames = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
 ];
 
-const days = ["SUN","MON","TUE","WED","THU","FRI","SAT"];
+const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
 class Calender extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             dates: {},
             eventDates: {},
-            month:0,
+            month: 0,
             year: 0,
             events: [],
             arr: [],
             startDate: new Date(),
-        }
+        };
         this.getCalendarDots = this.getCalendarDots.bind(this);
         this.getCalendarEventDots = this.getCalendarEventDots.bind(this);
         this.getEvents = this.getEvents.bind(this);
@@ -34,75 +45,68 @@ class Calender extends Component {
         this.getDateArray = this.getDateArray.bind(this);
     }
 
-    componentDidMount(){
-        const {startDate} = this.state;
-        let endDate = new Date(startDate.getTime()+(6*24*60*60*1000));
+    componentDidMount() {
+        const { startDate } = this.state;
+        let endDate = new Date(startDate.getTime() + 6 * 24 * 60 * 60 * 1000);
         let ar = this.getDateArray(startDate, endDate);
-        this.setState({arr: ar});
+        this.setState({ arr: ar });
         this.getCalendarDots();
         this.getCalendarEventDots();
         this.getEvents();
     }
-    
+
     formatDate = (date) => {
         var d = new Date(date);
         var month = '' + (d.getMonth() + 1);
         var day = '' + d.getDate();
         var year = d.getFullYear();
-    
-        if (month.length < 2) 
-            month = '0' + month;
-        if (day.length < 2) 
-            day = '0' + day;
-    
+
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+
         return [year, month, day].join('-');
-    }
+    };
 
-    getCalendarDots(){
-        getDots()
-        .then(resp => {
-            if(resp && resp.data){
-                this.setState({dates: resp.data}); 
+    getCalendarDots() {
+        getDots().then((resp) => {
+            if (resp && resp.data) {
+                this.setState({ dates: resp.data });
             }
         });
     }
-    
 
-    getCalendarEventDots(){
-        getEventDots()
-        .then(resp => {
-            if(resp && resp.data){
-                this.setState({eventDates: resp.data}); 
+    getCalendarEventDots() {
+        getEventDots().then((resp) => {
+            if (resp && resp.data) {
+                this.setState({ eventDates: resp.data });
             }
         });
     }
-    
-    getEvents(){
+
+    getEvents() {
         this.props.dispatch(addLoading());
         setTimeout(() => {
-            const {startDate} = this.state;
+            const { startDate } = this.state;
             let start_date = startDate;
-            let end_date = new Date(start_date.getTime()+(6*24*60*60*1000));
-            let ar = this.getDateArray(start_date,end_date);
-            this.setState({arr: ar});
+            let end_date = new Date(start_date.getTime() + 6 * 24 * 60 * 60 * 1000);
+            let ar = this.getDateArray(start_date, end_date);
+            this.setState({ arr: ar });
             start_date = this.formatDate(start_date);
             end_date = this.formatDate(end_date);
-            const params = {type: 'event', start_date, end_date};
-            getSchedule(params)
-            .then(resp => {
+            const params = { type: 'event', start_date, end_date };
+            getSchedule(params).then((resp) => {
                 console.log('resp', resp);
-                if(resp){
-                    this.setState({events: resp});
+                if (resp) {
+                    this.setState({ events: resp });
                     this.props.dispatch(removeLoading());
-                }else{
+                } else {
                     this.props.dispatch(removeLoading());
                 }
-            })
-        },1000);
-        
+            });
+        }, 1000);
     }
-    
-    getDateArray = function(start, end) {
+
+    getDateArray = function (start, end) {
         var arr = new Array();
         var dt = new Date(start);
         while (dt <= end) {
@@ -110,99 +114,111 @@ class Calender extends Component {
             dt.setDate(dt.getDate() + 1);
         }
         return arr;
-    }
-    
-    render(){
-    const {dates, startDate, arr, events, eventDates, month, year } = this.state;
-    let endDate = new Date(startDate.getTime()+(6*24*60*60*1000));
-    const current_date = new Date();
-    return (
-       <div>
-        <Loader></Loader>
-        <Card bordered={false} className="calender-card">
-            <div className="card-head">
-            <span>{(month == 0 && year == 0) ? monthNames[current_date.getMonth()] : monthNames[month]}{' '}{(month == 0 && year == 0) ? current_date.getFullYear() : year}</span>
-                <div className="btn-wrap">
-                    <Button className="fp-button-primary" type="primary" icon={<CalendarOutlined />}>
-                        <span>Go to Calender</span>
-                    </Button>
-                </div>
-            </div>
-            <DatePicker
-                labelFormat={'MMMM'}
-                color={'#374e8c'}
-                selectDate={startDate}
-                dates={dates}
-                eventDates={eventDates}
-                getSelectedDay={(val) => {
-                    console.log('val', val);
-                    this.setState({startDate: val});
-                    this.getEvents();
-                }}
-                onScrolled={(val) => {
-                    this.setState({month: new Date(val).getMonth(), year: new Date(val).getFullYear()})
-                }}
-            />
-            <div className="card-body-wrap">
-                <div className="name-wrap">
-                    <div className="circle blue">
-                        <span>S</span>
+    };
+
+    render() {
+        const { dates, startDate, arr, events, eventDates, month, year } = this.state;
+        let endDate = new Date(startDate.getTime() + 6 * 24 * 60 * 60 * 1000);
+        const current_date = new Date();
+        return (
+            <div className="loader-wrap">
+                <Loader></Loader>
+                <Card bordered={false} className="calender-card">
+                    <div className="card-head">
+                        <span>
+                            {month == 0 && year == 0 ? monthNames[current_date.getMonth()] : monthNames[month]}{' '}
+                            {month == 0 && year == 0 ? current_date.getFullYear() : year}
+                        </span>
+                        <div className="btn-wrap">
+                            <Button className="fp-button-primary" type="primary" icon={<CalendarOutlined />}>
+                                <span>Go to Calender</span>
+                            </Button>
+                        </div>
                     </div>
-                    <div className="circle orange">
-                        <span>S</span>
-                    </div>
-                    <div className="circle green">
-                        <span>S</span>
-                    </div>
-                    <div className="circle violet">
-                        <span>S</span>
-                    </div>
-                </div>
-                
-                <ScrollArea speed={0.8} className="list-wrapper" contentClassName="content" horizontal={false}>
-                    <div className="list">
-                      {arr && arr.map((date,i) => (
-                        <div className="list-item selected">  
-                            <div className="list-day">
-                                <span>{date.getDate()}</span>
-                                <span>{days[date.getDay()]}</span>
+                    <DatePicker
+                        labelFormat={'MMMM'}
+                        color={'#374e8c'}
+                        selectDate={startDate}
+                        dates={dates}
+                        eventDates={eventDates}
+                        getSelectedDay={(val) => {
+                            console.log('val', val);
+                            this.setState({ startDate: val });
+                            this.getEvents();
+                        }}
+                        onScrolled={(val) => {
+                            this.setState({ month: new Date(val).getMonth(), year: new Date(val).getFullYear() });
+                        }}
+                    />
+                    <div className="card-body-wrap">
+                        <div className="name-wrap">
+                            <div className="circle blue">
+                                <span>S</span>
                             </div>
-                            {events && events.map((event,i) => (
-                                    
-                                <div className="list-details">
-                                    {this.formatDate(date) == this.formatDate(new Date(event.start_date)) && event.Members && event.Members[0]  ? (
-                                    <div className="list-wrap">
-                                        <div className="list-timer">
-                                        <span>{event.start_time}</span>
-                                          
-                                            <div className="stat">
-                                            {event.Members && event.Members[0] && (    
-                                               <span  style={{background: event.Members[0].color}} className="list-info"></span>
-                                            )}
+                            <div className="circle orange">
+                                <span>S</span>
+                            </div>
+                            <div className="circle green">
+                                <span>S</span>
+                            </div>
+                            <div className="circle violet">
+                                <span>S</span>
+                            </div>
+                        </div>
+
+                        <ScrollArea speed={0.8} className="list-wrapper" contentClassName="content" horizontal={false}>
+                            <div className="list">
+                                {arr &&
+                                    arr.map((date, i) => (
+                                        <div className="list-item selected">
+                                            <div className="list-day">
+                                                <span>{date.getDate()}</span>
+                                                <span>{days[date.getDay()]}</span>
+                                            </div>
+                                            <div>
+                                                {events &&
+                                                    events.map((event, i) => (
+                                                        <div className="list-details">
+                                                            {this.formatDate(date) ==
+                                                                this.formatDate(new Date(event.start_date)) &&
+                                                            event.Members &&
+                                                            event.Members[0] ? (
+                                                                <div className="list-wrap">
+                                                                    <div className="list-main">
+                                                                        <div className="list-timer">
+                                                                            <span>{event.start_time}</span>
+                                                                            <div className="stat">
+                                                                                {event.Members && event.Members[0] && (
+                                                                                    <span
+                                                                                        style={{
+                                                                                            background:
+                                                                                                event.Members[0].color,
+                                                                                        }}
+                                                                                        className="list-info"
+                                                                                    ></span>
+                                                                                )}
+                                                                            </div>
+                                                                        </div>
+                                                                        <div className="list-activity">
+                                                                            <span>{event.title}</span>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            ) : null}
+                                                        </div>
+                                                    ))}
                                             </div>
                                         </div>
-                                        <div className="list-activity">
-                                            <span>{event.title}</span>
-                                        </div>
-                                    </div>
-                                    ): null}
-                                </div>
-
-                            ))} 
-                           </div>
-                      ))} 
-
+                                    ))}
+                            </div>
+                        </ScrollArea>
                     </div>
-                       
-                    </ScrollArea>    
+                </Card>
             </div>
-          
-        </Card>
-      </div>  
-    );
+        );
     }
-};
+}
 
-const mapStateToProps = ({ serviceReducer }) => ({serviceReducer});
+const mapStateToProps = ({ serviceReducer }) => ({ serviceReducer });
 
 export default connect(mapStateToProps)(Calender);
